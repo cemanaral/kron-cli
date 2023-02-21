5244c1f22da22395c07abf8bc1f7da2c69032998
Cem Anaral <cemanaral@hotmail.com>
2023-02-21 23:30:26 +0300

    refactor: remove Type keyword

--- pkg/host/host.go
+++ pkg/host/host.go
@@ -24,7 +24,6 @@ type HostConfig struct {
 type Host struct {
 	SshConn    sshutil.SshConnection
 	HostConfig `yaml:",inline"`
-	Type       string
 }
 
 func (h HostConfig) GetCronInformation() string {
