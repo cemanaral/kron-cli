e553a40726ac8f09efb020abb63bafed7a0ca147
Cem Anaral <canaral@kocfinans.com.tr>
2023-02-21 17:03:35 +0300

    refactor: remove HostList data type

--- cmd/get.go
+++ cmd/get.go
@@ -12,24 +12,24 @@ import (
 var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Retrieve cron informations from hosts",
-	Long: `Retrieve cron informations from hosts`,
+	Long:  `Retrieve cron informations from hosts`,
 	Run: func(cmd *cobra.Command, args []string) {
 		get(args)
 	},
 }
 
 func get(args []string) {
-	selectedHosts := make(host.HostList, len(args), len(args))
+	selectedHosts := make([]host.Host, len(args), len(args))
 	for i, hostName := range args {
-		hostIndex := slices.IndexFunc(host.Hosts, func(h host.Host) bool {return h.Id == hostName}); 
+		hostIndex := slices.IndexFunc(host.Hosts, func(h host.Host) bool { return h.Id == hostName })
 		if hostIndex == -1 {
 			log.Fatalf("Host '%s' could not be found in %s. Please validate your configuration by executing 'kron hosts'", hostName, host.HOSTS_PATH)
 		}
-		selectedHosts[i] = host.Hosts[hostIndex] 
+		selectedHosts[i] = host.Hosts[hostIndex]
 	}
 	for _, h := range selectedHosts {
 		h.GetCronInformation()
-	}	
+	}
 }
 
 func init() {
--- pkg/host/host.go
+++ pkg/host/host.go
@@ -10,62 +10,61 @@ import (
 )
 
 const HOSTS_PATH = "/etc/kron/hosts.yaml"
-var Hosts = HostList{};
 
-type HostList []Host
+var Hosts = []Host{}
 
 type HostConfig struct {
-     Id string `yaml:"host"`
-     Address string `yaml:"address"`
-     User string `yaml:"user"`
-     Password string `yaml:"password"`
-     PrivateKeyPath string `yaml:"private-key"`
+	Id             string `yaml:"host"`
+	Address        string `yaml:"address"`
+	User           string `yaml:"user"`
+	Password       string `yaml:"password"`
+	PrivateKeyPath string `yaml:"private-key"`
 }
 
 type Host struct {
-     SshConn sshutil.SshConnection
-     HostConfig `yaml:",inline"`
-     Type string
+	SshConn    sshutil.SshConnection
+	HostConfig `yaml:",inline"`
+	Type       string
 }
 
 func (h HostConfig) GetCronInformation() string {
-     if h.Password != "" {
-          fmt.Println(h.Id, "password is chosen")
-     } else if h.PrivateKeyPath != "" {
-          fmt.Println(h.Id, "private key method is chosen")
-     }
-     log.Fatalf("No valid authentication method was selected for '%s'!", h.Id)
-     return ""
+	if h.Password != "" {
+		fmt.Println(h.Id, "password is chosen")
+	} else if h.PrivateKeyPath != "" {
+		fmt.Println(h.Id, "private key method is chosen")
+	}
+	log.Fatalf("No valid authentication method was selected for '%s'!", h.Id)
+	return ""
 }
 
-func (hosts *HostList ) load() {
-     yfile, err := ioutil.ReadFile(HOSTS_PATH)
-     if err != nil {
-          log.Fatal(err)
-     }
-     err2 := yaml.Unmarshal(yfile, hosts)
-     if err2 != nil {
-          log.Fatal(err2)
-     }
+func LoadHosts() {
+	yfile, err := ioutil.ReadFile(HOSTS_PATH)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err2 := yaml.Unmarshal(yfile, &Hosts)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	for i := 0; i < len(Hosts); i++ {
+		Hosts[i].determineAuthMethod()
+	}
+
 }
 
-func (h Host)determineAuthMethod() {
-     if h.Password != "" {
-          h.SshConn = sshutil.SshConnectionWithPassword{}
-     } else if h.PrivateKeyPath != "" {
-          h.SshConn = sshutil.SshConnectionWithPrivateKey{}
-     }
+func (h *Host) determineAuthMethod() {
+	if h.Password != "" {
+		h.SshConn = sshutil.SshConnectionWithPassword{}
+	} else if h.PrivateKeyPath != "" {
+		h.SshConn = sshutil.SshConnectionWithPrivateKey{}
+	}
 }
 
 func (host Host) String() string {
-     return fmt.Sprintf(`host -> %s
+	return fmt.Sprintf(`host -> %s
 address -> %s
 user -> %s
 password -> %s
 private-key -> %s
 `, host.Id, host.Address, host.User, host.Password, host.PrivateKeyPath)
-}
-
-func LoadHosts() {
-     Hosts.load()
 }
